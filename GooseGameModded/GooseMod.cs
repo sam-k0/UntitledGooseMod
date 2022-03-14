20756caf30ba51d10b808b08d2376f12e741a3fb
Samuel Koob <56673835+sam-k0@users.noreply.github.com>
2022-03-14 22:29:46 +0100

    Add project files.

---
+++ GooseGameModded/GooseMod.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GooseGameModded
+{
+    class GooseMod : MonoBehaviour
+    {
+        // Alloc vars
+        const string VERSIONSTR = "1.2";
+        Goose goose = null;
+        string sceneName = "undefined";
+        bool gooseFound = false;
+
+        String objectListString = "Press 1 to scan";
+
+        public void OnGUI() // Draw-Step
+        {
+            // show current scene
+            GUI.Label(new Rect(0f, 0f, 200f, 60f), "UntitledGooseMod" + VERSIONSTR +"\n" + " | Scene: " + sceneName + "\n | Goose found: " + Convert.ToString(gooseFound));
+
+            // Show FPS
+            GUI.Label(new Rect(0f, 60f, 200f, 120f), "Performance: " + Convert.ToString(1.0f / Time.deltaTime));
+
+            // Obj List
+            GUI.Label(new Rect(0f, 120f, 500f, 1000f), objectListString);
+        }
+
+        public void Start() // On-Create
+        {
+            
+        }
+
+        public void Update() // Step event
+        {
+            if (Input.GetKeyDown(KeyCode.Delete)) // Unload from Game
+            {
+                Loader.Unload();
+            }
+
+            if( Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                // Get scene name
+                Scene cScene = SceneManager.GetActiveScene();
+                sceneName = cScene.name;
+
+                // Get goose
+                goose = FindObjectOfType<Goose>();
+                if(goose != null)
+                {
+                    MeshRenderer renderer = goose.GetComponent<MeshRenderer>();
+                    Vector3 gooseSize = renderer.bounds.size;
+                    gooseSize.x += 3;
+                    gooseFound = true;
+                }
+                else
+                {
+                    gooseFound = false;
+                }
+                
+            }
+
+            if(Input.GetKeyDown(KeyCode.Alpha1)) // Scan all objects
+            {
+                GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+                foreach (GameObject go in allObjects)
+                {
+                    if (go.activeInHierarchy)
+                    {
+                        objectListString += go.name + "|";
+                    }
+                }
+                   
+            }
+        }
+    }
+}
---
+++ GooseGameModded/Loader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GooseGameModded
+{
+    public class Loader
+    {
+        private static GameObject Load;
+        public static void Init()
+        {
+            Loader.Load = new GameObject();
+            Loader.Load.AddComponent<GooseMod>();
+            UnityEngine.Object.DontDestroyOnLoad(Loader.Load); // Do not throw away immediately
+        }
+
+        public static void Unload()
+        {
+            _Unload();
+        }
+
+        private static void _Unload()
+        {
+            GameObject.Destroy(Load);
+        }
+    }
+}
